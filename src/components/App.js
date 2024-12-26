f82a04bef296c77e5fd8676e247322ceab1ff6ef
vibraniumSwaleh <hus.swaleh@yahoo.com>
2024-12-26 19:36:36 +0000

    render buttons on answer

--- src/components/App.js
+++ src/components/App.js
@@ -11,6 +11,7 @@ const initialState = {
   // loading, error, ready, active, finished
   status: 'loading',
   index: 0,
+  answer: null,
 };
 
 function reducer(state, action) {
@@ -25,13 +26,15 @@ function reducer(state, action) {
       return { ...state, status: 'error' };
     case 'start':
       return { ...state, status: 'active' };
+    case 'newAnswer':
+      return { ...state, answer: action.payload };
     default:
       throw new Error('Action unknown');
   }
 }
 
 function App() {
-  const [{ questions, status, index }, dispatch] = useReducer(
+  const [{ questions, status, index, answer }, dispatch] = useReducer(
     reducer,
     initialState,
   );
@@ -62,7 +65,13 @@ function App() {
         {status === 'ready' && (
           <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
         )}
-        {status === 'active' && <Question question={questions[index]} />}
+        {status === 'active' && (
+          <Question
+            question={questions[index]}
+            dispatch={dispatch}
+            answer={answer}
+          />
+        )}
       </Main>
     </div>
   );
--- src/components/Options.js
+++ src/components/Options.js
@@ -1,8 +1,22 @@
-function Options({ question }) {
+function Options({ question, dispatch, answer }) {
+  const hasAnswered = answer !== null;
+
   return (
     <div className='options'>
-      {question.options.map((option) => (
-        <button key={option} className='btn btn-option'>
+      {question.options.map((option, index) => (
+        <button
+          key={option}
+          disabled={hasAnswered}
+          className={`btn btn-option ${index === answer ? 'answer' : ''} 
+          ${
+            hasAnswered
+              ? index === question.correctOption
+                ? 'correct'
+                : 'wrong'
+              : ''
+          }`}
+          onClick={() => dispatch({ type: 'newAnswer', payload: index })}
+        >
           {option}
         </button>
       ))}
