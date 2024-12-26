f98385af5fafaafc3d9f927095bfe91d028bffbd
vibraniumSwaleh <hus.swaleh@yahoo.com>
2024-12-26 14:39:27 +0000

    add ui components and useReducer

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import { useEffect } from 'react';
+import { useEffect, useReducer } from 'react';
 import Header from './Header';
-import './index.css';
 import Main from './Main';
+import Loader from './Loader';
+import Error from './Error';
+import StartScreen from './StartScreen';
+
+const initialState = {
+  questions: [],
+  // loading, error, ready, active, finished
+  status: 'loading',
+};
+function reducer(state, action) {
+  switch (action.type) {
+    case 'dataReceived':
+      return {
+        ...state,
+        questions: action.payload,
+        status: 'ready',
+      };
+    case 'dataFailed':
+      return { ...state, status: 'error' };
+    default:
+      throw new Error('Action unknown');
+  }
+}
 
 function App() {
+  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+
+  const numQuestions = questions.length;
+
   useEffect(function () {
     async function getQuestions() {
-      const res = await fetch('http://localhost:8000/questions');
-      const data = await res.json();
-      console.log(data);
+      try {
+        const res = await fetch('http://localhost:8000/questions');
+        const data = await res.json();
+        dispatch({ type: 'dataReceived', payload: data });
+      } catch (error) {
+        dispatch({ type: 'dataFailed' });
+      }
     }
 
     getQuestions();
   }, []);
 
   return (
-    <div>
+    <div className='app'>
       <Header />
 
       <Main>
-        <p>1/15</p>
-        <p>Question?</p>
+        {status === 'loading' && <Loader />}
+        {status === 'error' && <Error />}
+        {status === 'ready' && <StartScreen numQuestions={numQuestions} />}
       </Main>
     </div>
   );
